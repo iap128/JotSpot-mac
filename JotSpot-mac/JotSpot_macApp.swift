1bb0e6d7339f966ed73118fb3b5ce2aaf9e1432f
Ryan Hunter <Ryan.Hunter@nike.com>
2026-01-12 12:53:00 -0800

    Added release notes window

--- JotSpot-mac/JotSpot_macApp.swift
+++ JotSpot-mac/JotSpot_macApp.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import AppKit
+
+private var releaseNotesWindowController: NSWindowController?
+
 
 @main
 struct JotSpot_macApp: App {
+	@Environment(\.openWindow) private var openWindow
+	
     var body: some Scene {
         DocumentGroup(newDocument: JotSpot_macDocument()) { file in
             ContentView(document: file.$document)
             .frame(minWidth: 500, minHeight: 500)
         }
+		.defaultSize(width: 500, height: 500)
+        .commands {
+            CommandGroup(after: .help) {
+				Button("Release Notes") {
+					openWindow(id: "release-notes")
+				}
+				
+                Button("Support") {
+                    if let url = URL(string: "https://n818pe.com/contact") {
+                        NSWorkspace.shared.open(url)
+                    }
+                }
+
+                Button("JotSpot Website") {
+                    if let url = URL(string: "https://n818pe.com/jotspot-mac") {
+                        NSWorkspace.shared.open(url)
+                    }
+                }
+            }
+        }
+		
+		
+		Window("Release Notes", id: "release-notes") {
+			ReleaseNotes()
+		}
+		.windowResizability(.contentSize)
     }
 }
---
+++ JotSpot-mac/ReleaseNotes.swift
@@ -0,0 +1,61 @@
+//
+//  ReleaseNotes.swift
+//  JotSpot-mac
+//
+//  Created by Ryan Hunter on 1/12/26.
+//
+
+import SwiftUI
+
+struct ReleaseNotesType: Hashable {
+	let image: String
+	let title: String
+	let body: String
+}
+
+let notes: [ReleaseNotesType] = [
+	ReleaseNotesType(image: "sparkles", title: "Redesign", body: "Completely redesigned to support macOS 26 and Apple Silicon")
+]
+
+struct ReleaseNotes: View {
+	@Environment(\.dismiss) private var dismiss
+	let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+	
+	
+    var body: some View {
+		VStack() {
+			Text("What's New in " + (appVersion ?? ""))
+				.font(.title)
+			
+			Divider()
+			
+			ForEach(notes, id: \.self) { note in
+				HStack {
+					Image(systemName: note.image)
+						.resizable()
+						.frame(width: 32, height: 32)
+						.foregroundStyle(.blue)
+					
+					VStack(alignment: .leading) {
+						Text(note.title)
+							.font(.title2)
+						
+						Text(note.body)
+					}
+					.padding(.leading)
+				}
+			}
+			
+			Button("Close") {
+				dismiss()
+			}
+			
+			Spacer()
+		}
+		.padding()
+    }
+}
+
+#Preview {
+    ReleaseNotes()
+}
